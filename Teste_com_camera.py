a5bf5a05c40c15ddfaeab6bd94c6f21f7902ea8f
ricardoBpinheiro <ricardo.pinheiro@catolicasc.org.br>
2020-06-02 00:04:15 -0300

    Teste com imagem

---
+++ Teste_com_camera.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2 as cv
+
+face_classifier = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+image = cv.imread('lena.jpg')
+image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+faces = face_classifier.detectMultiScale(image_gray, 1.3, 5)
+
+for (x, y, w, h) in faces:
+    cv.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+cv.imshow('imagem', image)
+cv.waitKey(0)
+cv.destroyAllWindows()
